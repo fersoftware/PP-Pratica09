f4dfaa0eb9683a5612010a2721f7106725f5735d
FERNANDO ALVES DA SILVA <fersoftware@gmail.com>
2016-05-09 21:53:16 -0300

    Add project files.

---
+++ PP-Pratica09/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Pratica09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
